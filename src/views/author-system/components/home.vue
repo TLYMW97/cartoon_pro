d5892a974e6e03eeeef8d15a8515b4773a125682
Link <751112877@qq.com>
2020-02-12 18:00:13 +0800

    阿西吧脖子疼

---
+++ src/views/author-system/components/home.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div class="top">
+            <div class="left-info">111</div>
+            <div class="right-msg">11</div>
+        </div>
+        <div class="my-cartoon">
+            <div class="header">
+                <h3>
+                    <strong>我的漫画</strong>
+                    （共有<span>7</span>部）
+                </h3>
+            </div>
+            <div class="cartoon-list">
+                111
+                111
+                11
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    };
+</script>
+
+<style scoped>
+    .clear{
+        clear: both;
+    }
+    .top{
+        margin: 25px 0 25px 0;
+        display: flex;
+    }
+    .top .left-info,.top .right-msg{
+        background: #f7f7f7;
+        width: 480px;
+        height: 165px;
+        padding: 20px;
+        margin-right: 25px;
+        border: #d6d6d6 solid 1px;
+    }
+    .my-cartoon{
+        border: #d6d6d6 solid 1px;
+        width: 985px;
+        height: 440px;
+        margin-top: 30px;
+        overflow-y: auto;
+    }
+    .my-cartoon .header{
+        position: absolute;
+        width: 983px;
+        height: 30px;
+        padding-bottom: 40px;
+        background: url("http://css99tel.cdndm5.com/v201910292122/cartoonupload/images/topbg.png") repeat-x top left;
+    }
+    .header h3{
+        width: 200px;
+        padding: 10px 30px 0;
+        background: #fff;
+        height: 38px;
+        border-right: 1px solid #dadada;
+        border-top: 1px solid #292929;
+    }
+    .my-cartoon .cartoon-list{
+        width: 100%;
+        height: 500px;
+        padding: 20px;
+    }
+</style>
--- src/views/author-system/index/index.vue
+++ src/views/author-system/index/index.vue
@@ -40,7 +40,42 @@
                 </div>
             </div>
             <div class="container">
-                <div class="center"></div>
+                <div class="center">
+                    <div class="left-nav">
+                        <div class="home">
+                            <img src="http://css99tel.cdndm5.com/v201910292122/cartoonupload/images/icon_leftnav_home.png" alt="">
+                            <router-link to="">首页</router-link>
+                        </div>
+                        <div class="admin-list">
+                            <ul>
+                                <li>
+                                    <img src="http://css99tel.cdndm5.com/v201910292122/cartoonupload/images/icon_leftnav_setting.png" alt="">
+                                    <router-link to="">漫画管理</router-link>
+                                </li>
+                                <li class="child-list">
+                                    <router-link to="">发表漫画</router-link>
+                                </li>
+                                <li class="child-list">
+                                    <router-link to="">未通过的章节</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="my-cartoon">
+                            <ul>
+                                <li>
+                                    <img src="http://css99tel.cdndm5.com/v201910292122/cartoonupload/images/icon_leftnav_my.png" alt="">
+                                    <router-link to="">我的漫画</router-link>
+                                </li>
+                                <li class="child-list">
+                                    <router-link to="">1111</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <component :is="comName"></component>
+                    </div>
+                </div>
             </div>
             <div class="footer"></div>
         </div>
@@ -48,8 +83,17 @@
 </template>
 
 <script>
+    import Home from '../components/home';
     export default {
         name: "index",
+        components:{
+          Home,
+        },
+        data(){
+            return{
+                comName:Home,
+            };
+        },
         methods: {
             logout(){
                 console.log(1);
@@ -115,14 +159,68 @@
     }
     .container{
         width: 100%;
-        height: 730px;
+        height: 100%;
     }
     .container .center{
         width: 1200px;
-        height: 100%;
+        height: 700px;
         background: white;
         border-radius: 15px;
         margin: 0 auto;
+    }
+    .center .left-nav{
+        float: left;
+        border-right: #d6d6d6 solid 1px;
+        border-radius: 15px 0 0 15px;
+        width: 157px;
+        height: 700px;
+        background: #f7f7f7;
+    }
+    .left-nav a:hover,.left-nav a:active{
+        color: #ff6000;
+    }
+    .left-nav a{
+        text-decoration: none;
+        color: #7c7c7c;
+        display: block;
+        margin-left: 10px;
+        height: 25px;
+        line-height: 25px;
+        font: 16px "Microsoft YaHei";
+        font-weight: bold;
+    }
+    .left-nav .home{
+        padding: 20px 0 20px 20px;
+        height: 65px;
+        border-bottom: #d6d6d6 solid 1px;
+        display: flex;
+    }
+    .left-nav .admin-list{
+        padding: 20px 0 0 20px;
+        border-bottom: #d6d6d6 solid 1px;
+    }
+    .admin-list ul, .my-cartoon ul{
+        list-style: none;
+        margin-bottom: 0;
+    }
+    .admin-list ul li, .my-cartoon ul li{
+        display: flex;
+        margin-bottom: 20px;
+    }
+    .child-list a{
+        margin-left: 35px;
+        display: block;
+        height: 25px;
+        line-height: 25px;
+        font-weight: normal;
+        font-size: 14px;
+    }
+    .left-nav .my-cartoon{
+        padding: 20px 0 0 20px;
+    }
+    .center .right{
+        padding-left: 24px;
+        float: left;
     }
     .footer{
         width: 100%;
